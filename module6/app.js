10117ae26e0702d861a9bb0f40314dc3a6857cdc
jonlee107 <jonlee87@gmail.com>
2016-11-03 01:32:15 -0400

    Add files via upload

--- module6/app.js
+++ module6/app.js
@@ -3,8 +3,30 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', function ($scope) {
-	$scope.name = "Jon";
-});
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController ($scope) {
+	$scope.lunchItems = "";
+	
+	$scope.displayCount = function () {
+		var lunchString = $scope.lunchItems;
+		if (lunchString == "") {
+			$scope.message = "Please enter data first";
+		}
+		else {
+			var arrayOfStrings = $scope.lunchItems.split(','); // get total value
+			var count = arrayOfStrings.length;
+		
+		
+			if (count <=3) {
+				$scope.message = "Enjoy!";
+			}
+			else {
+				$scope.message = "Too much!";
+			}
+		}
+	};
+};
 
 })();
