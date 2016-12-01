b5f84575df87483818759517ec5d6b6ce7683fa8
Jon <jonlee87@gmail.com>
2016-11-30 23:55:10 -0500

    Upload initial files

---
+++ module9/menudata.service.js
@@ -0,0 +1,50 @@
+(function () {
+
+
+MenuDataService
+
+// Service is called
+// the function value is named
+
+angular.module('data', [])
+  .service('')
+
+  getAllCategories // return promise of what happens/happened 
+
+
+function getAllCategories() {
+
+};
+
+function getItemsForCategory(categoryShortName) {
+
+};
+
+
+
+angular.module('data',['ui.router']);
+
+angular.module('RoutingApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to tab 1 if no other URL matches
+  $urlRouterProvider.otherwise('/tab1');
+
+  // Set up UI states
+  $stateProvider
+    .state('tab1', {
+      url: '/tab1',
+      templateUrl: 'src/tab1.html'
+    })
+
+    .state('tab2', {
+      url: '/tab2',
+      templateUrl: 'src/tab2.html'
+    });
+}
+
+
+})();
